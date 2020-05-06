55e1ebd87f97d53d02246505d0a0be6652cfd1dc
Yu SERIZAWA <upamune@gmail.com>
2020-05-07 01:06:32 +0900

    added recipe for secrets

--- recipe.rb
+++ recipe.rb
@@ -76,13 +76,46 @@ go_packages.each do |p|
   end
 end
 
+execute "Copy go tools to /usr/local/bin" do
+    command "cp -fr $(go env GOPATH)/bin/* /usr/local/bin/"
+end
+
+execute "Install 1Password CLI" do
+  command <<-"EOS"
+    export OP_VERSION="v0.10.0"
+    curl -sS -o 1password.zip https://cache.agilebits.com/dist/1P/op/pkg/${OP_VERSION}/op_linux_amd64_${OP_VERSION}.zip && \
+    unzip 1password.zip op -d /usr/local/bin && \
+    rm -f 1password.zip
+  EOS
+  not_if "test -e /usr/local/bin/op"
+end
+
+pull_secret_content = <<-"EOS"
+#!/bin/bash -eu
+echo "Authenticating with 1Password"
+export OP_SESSION_my=$(op signin https://my.1password.com upamune@gmail.com --output=raw)
+export OP_VAULT="remote-workstation"
+echo "Pulling secrets"
+op get document 'id_ed25519' --vault "${OP_VAULT}" > id_ed25519
+op get document 'gpg.private' --vault "${OP_VAULT}" > gpg.private
+op get document 'gpg.public' --vault "${OP_VAULT}" > gpg.public
+rm -f ~/.ssh/id_ed25519
+mkdir -p ~/.ssh
+chmod 0700 ~/.ssh
+ln -sfn $(pwd)/id_ed25519 ~/.ssh/id_ed25519
+chmod 0600 ~/.ssh/id_ed25519
+gpg --import gpg.private
+gpg --import gpg.public
+echo "Done!"
+EOS
+
+execute "Create pull-secret.sh" do
+  command "echo '#{pull_secret_content}' > pull-secrets.sh && chmod +x pull-secrets.sh"
+end
+
 execute "Install SpaceVim" do
   command "curl -sLf https://spacevim.org/install.sh | bash"
   not_if "test -e /root/.SpaceVim"
-end
-
-execute "Copy go tools to /usr/local/bin" do
-    command "cp -fr $(go env GOPATH)/bin/* /usr/local/bin/"
 end
 
 execute "Set correct timezone" do
